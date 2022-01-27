4c6c7e473ce1acc77aa95c6b64e94b60266dd13b
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-01-27 22:33:58 +0100

    Update version date

--- AceQLClient/src/Api.Util/VersionValues.cs
+++ AceQLClient/src/Api.Util/VersionValues.cs
@@ -25,6 +25,6 @@ namespace AceQL.Client.Api.Util
     {
         internal static readonly String PRODUCT = "AceQL HTTP Client SDK";
         internal static readonly String VERSION = "v7.4";
-        internal static readonly String DATE = "13-Dec-2021";
+        internal static readonly String DATE = "21-Jan-2022";
     }
 }
