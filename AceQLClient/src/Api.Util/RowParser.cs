f47c0e61be08963cc07a3be6b5490dc1a8d7d366
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-04-15 20:15:22 +0200

    Copyright is now 2022

--- AceQLClient/src/Api.Util/RowParser.cs
+++ AceQLClient/src/Api.Util/RowParser.cs
@@ -1,7 +1,7 @@
 /*
  * This filePath is part of AceQL C# Client SDK.
  * AceQL C# Client SDK: Remote SQL access over HTTP with AceQL HTTP.                                 
- * Copyright (C) 2021,  KawanSoft SAS
+ * Copyright (C) 2022,  KawanSoft SAS
  * (http://www.kawansoft.com). All rights reserved.                                
  *                                                                               
  * Licensed under the Apache License, Version 2.0 (the "License");
--- AceQLClient/src/Api/ConnectionInfo.cs
+++ AceQLClient/src/Api/ConnectionInfo.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * This file is part of AceQL JDBC Driver.
  * AceQL JDBC Driver: Remote JDBC access over HTTP with AceQL HTTP.
- * Copyright (C) 2021,  KawanSoft SAS
+ * Copyright (C) 2022,  KawanSoft SAS
  * (http://www.kawansoft.com). All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
