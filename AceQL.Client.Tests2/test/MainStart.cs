eb1d2a13a59e8a3319351cc1a5c91024f10c4403
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-07-10 19:24:19 +0200

    MainStart clean

--- AceQL.Client.Tests2/test/MainStart.cs
+++ AceQL.Client.Tests2/test/MainStart.cs
@@ -61,7 +61,7 @@ namespace AceQL.Client.Test
 
         public static void Main(string[] args)
         {
-            int mainToLaunch = 14;
+            int mainToLaunch = 0;
 
             if (mainToLaunch == 0)
             {
